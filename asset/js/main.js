f96a819d2736ef5cde9f5150f1295c1d1a8e6de9
blawidris <blawidris@gmail.com>
2022-05-29 18:47:12 -0700

    all wallet added

---
+++ asset/js/main.js
@@ -0,0 +1,54 @@
+(function () {
+    ("use strict");
+  
+    let select = (el, all = false) => {
+      el = el.trim();
+      if (all) {
+        return [...document.querySelectorAll(el)];
+      } else {
+        return document.querySelector(el);
+      }
+    };
+  
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+      if (all) {
+        select(el, all).forEach((e) => e.addEventListener(type, listener));
+      } else {
+        select(el, all).addEventListener(type, listener);
+      }
+    };
+  
+    /**
+     * Easy on scroll event listener
+     */
+    const onscroll = (el, listener) => {
+      el.addEventListener("scroll", listener);
+    };
+
+    // get img
+    let box = select('.blockchain-item', true);
+    let boxImg = select('.blockchain .img', true);
+    let boxTitle = select('.blockchain h4', true);
+
+    boxImg.forEach((e,index)=>{
+        box.forEach((a, i)=>{
+            if(index === i){
+                a.dataset.img = e.src
+            }
+        })
+    })
+    boxTitle.forEach((e,index)=>{
+        box.forEach((a, i)=>{
+            if(index === i){
+                a.dataset.name = e.textContent
+            }
+        })
+    })
+
+   on('click', '.box', ()=>{
+       
+   }, true);
+})();
